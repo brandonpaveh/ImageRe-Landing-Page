658b4971dc98f08f8597ff4b022217e755784b7a
Brandon Paveh <brandonpaveh@Brandons-MacBook-Air.local>
2026-04-27 22:25:43 -0700

    Revert "Integrate Vercel Analytics React component"
    This reverts commit 3f210c4e6004f3c34da2569a9362e1243ccab367.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import { ErrorBoundary } from './ErrorBoundary';
 import './index.css';
@@ -9,7 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
       <App />
-      <Analytics />
     </ErrorBoundary>
   </React.StrictMode>
 );
